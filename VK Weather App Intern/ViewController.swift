54c31aa29b8bcc4e6ba2c94d86ab594fff77713d
artyom <tomas.tomson@mail.ru>
2024-03-20 21:50:03 +0300

    Initial Commit

---
+++ VK Weather App Intern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK Weather App Intern
+//
+//  Created by artyom s on 20.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
