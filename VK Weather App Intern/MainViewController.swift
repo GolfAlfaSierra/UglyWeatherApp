410fd5c64cea0dfbf3f267beba42da94570c6e84
artyom <tomas.tomson@mail.ru>
2024-03-23 22:45:25 +0300

    main screen intial viewcontroller preparations

--- VK Weather App Intern/MainViewController.swift
+++ VK Weather App Intern/MainViewController.swift
@@ -7,32 +7,80 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+struct MainViewModel {
+    
+    let currentDateTime: String
+    let cityName: String
+    let temperature: String
+    let weatherDescription: String
+    let lon: String
+    let lat: String
+    let weatherDetail: WeatherDetail
+    var weatherDetailShort = [WeatherDetailShort]()
+    
+    struct WeatherDetail {
+        let lowestTemperature: String
+        let highestTemperature: String
+        let feelsLike: String
+        let sealevel: String
+        let pressure: String
+        let cloudiness: String
+        let precipation: String
+        let visibility: String
+        let humidity: String
+        let wind: String
+    }
+    
+    struct WeatherDetailShort {
+        let dayOfWeek:String
+        let temperature:String
+        let lowestTemperature:String
+        let highestTemperature:String
+        let precepiation: String
+    }
+    
+    
+}
+
+final class MainViewController: UIViewController {
+    
+    let collectionview = MainView()
+    
     
     override func loadView() {
-        view = MainView()
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view = collectionview
+        
     }
     
 }
 
-class MainView: UIView {
+final class MainView: UICollectionView {
+    
+    
     init() {
-        super.init(frame: .zero)
-        backgroundColor = .white
+        let layout = UICollectionViewFlowLayout()
+        super.init(frame: .zero, collectionViewLayout: layout)
+        configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func configure() {
+        backgroundColor = .background
+            
+        register(HeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HeaderView.resueId)
+    }
 }
 
 
+
 #Preview {
-    MainViewController()
+    
+    return MainViewController()
+}
+#Preview {
+    
+    return HeaderView()
 }
